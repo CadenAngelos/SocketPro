fd265a514ad5a8ca86681438fe90b4bfa7694a41
hachihao792001 <hachihao792001@gmail.com>
2020-12-17 01:00:47 +0700

    sửa lại code cho gọn gàng
    - để hết các hàm Move thành 1 hàm Move tổng quát
    - để hết các hàm SendFile thành 1 hàm SendFile tổng quát
    - Thêm hàm ReadRequestAndSendFile, để cho tên hàm hợp với mục đích của hàm

--- Server.py
+++ Server.py
@@ -1,7 +1,5 @@
-# #%% console  shift enter
+# #%% console shift enter
 import socket
-import time
-
 
 def createServer(host, port):
     Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,48 +30,14 @@ def ReadHTTPRequest(Server):
         re = readRequest(Client)
     return Client, re
 
-
-def SendFileIndex(Client):
-    f = open("index.html", "rb")
-    L = f.read()
-    header = """HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-length: %d\r\n\r\n"""%len(L)
-    print("----------- HTTP response index.html: ")
-    print(header)
-    header += L.decode()
-    header += "\n\n\n"
-    Client.send(bytes(header, 'utf-8'))
-    f.close()
-
-def SendIndexCSS(Server, Client):
-    Client, Request = ReadHTTPRequest(Server)
-    print("HTTP Request: ")
-    print(Request)
-    if "GET /styleIndex.css HTTP/1.1" in Request:
-        f = open("styleIndex.css", "rb")
-        L = f.read()
-        header = """HTTP/1.1 200 OK\r\nContent-Type: text/css; charset=UTF-8\r\nContent-Encoding: UTF-8\r\nContent-Length: %d\r\n\r\n"""%len(L)
-        print("-------------HTTP response  styleIndex.css: ")
-        print(header)
-        header += L.decode()
-        header += "\r\n"
-        Client.send(header.encode('utf-8')) 
-
-def MovePageIndex(Client):
-    header = """HTTP/1.1 301 Moved Permanently\r\nLocation: http://127.0.0.1:1235/index.html \n\n\n"""
-    print(">")
-    print("----------- HTTP response move Index.html:")
-    print(header)
-    Client.send(bytes(header, 'utf-8'))
-
-
 def MoveHomePage(Server, Client, Request):
     if "GET /index.html HTTP/1.1" in Request:
-        SendFileIndex(Client)
-        SendIndexCSS(Server, Client)
+        SendFile(Client, Request, GetFileNameFromRequest(Request)) #index.html
+        ReadRequestAndSendFile(Server) #styleIndex.css
         Server.close()
         return True
     if "GET / HTTP/1.1" in Request:
-        MovePageIndex(Client)
+        MoveToPage(Server, Client, "http://127.0.0.1:1235/index.html")
         Server.close()
 
         Server = createServer("localhost", 1235)
@@ -83,7 +47,6 @@ def MoveHomePage(Server, Client, Request):
         MoveHomePage(Server, Client, Request)
         return True
 
-
 def CheckPass(Request):
     if "POST / HTTP/1.1" not in Request:
         return False
@@ -92,125 +55,54 @@ def CheckPass(Request):
     else:
         return False
 
+def GetFileNameFromRequest(Request):
+    return Request[5:Request.find(" HTTP/1.1")] #GET /file.html HTTP/1.1...
 
-def Move404(Server, Client):
-    header = """HTTP/1.1 301 Moved Permanently\r\nLocation: http://127.0.0.1:1236/404.html \n\n\n"""
-    print("HTTP response: ")
-    print(header)
-    Client.send(bytes(header, "utf-8"))
-    Server.close()
-
-def Send404CSS(Server, Client):
-    Client, Request = ReadHTTPRequest(Server)
-    print("HTTP Request: ")
-    print(Request)
-    if "GET /style404.css HTTP/1.1" in Request:
-        f = open("style404.css", "rb")
-        L = f.read()
-        header = """HTTP/1.1 200 OK\r\nContent-Type: text/css; charset=UTF-8\r\nContent-Encoding: UTF-8\r\nContent-Length: %d\r\n\r\n"""%len(L)
-        print("-------------HTTP response  style404.css: ")
-        print(header)
-        header += L.decode()
-        header += "\r\n"
-        Client.send(header.encode('utf-8')) 
-
-def Send404(Server, Client):
-    Client, Request = ReadHTTPRequest(Server)
-    print("HTTP Request: ")
-    print(Request)
-    if "GET /404.html HTTP/1.1" in Request:
-        f = open("404.html", "rb")
-        L = f.read()
-        header = """HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Encoding: UTF-8\r\nContent-Length: %d\r\n\r\n"""%len(L)
-        print("HTTP response file 404.html: ")
-        print(header)
-        header += L.decode()
-        header += "\n\n\n"
-        Client.send(bytes(header, 'utf-8'))
-
-
-def MoveInfo(Server, Client):
-    header = """HTTP/1.1 301 Moved Permanently\r\nLocation: http://127.0.0.1:1236/info.html \n\n\n
-    """
+def MoveToPage(Server, Client, location):
+    header = "HTTP/1.1 301 Moved Permanently\r\nLocation: "
+    header += location
+    header += "\n\n\n"
     print("HTTP response: ")
     print(header)
     Client.send(bytes(header, 'utf-8'))
     Server.close()
 
-def SendInfo(Server, Client):
+def ReadRequestAndSendFile(Server):
     Client, Request = ReadHTTPRequest(Server)
-    print("HTTP Request: ")
+    print("-------------HTTP Request: ")
     print(Request)
-    if "GET /info.html HTTP/1.1" in Request:
-        f = open("info.html", "rb")
-        L = f.read()
-        header = """HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Encoding: UTF-8\r\nContent-Length: %d\r\n\r\n"""%len(L)
-        print("-------------HTTP response  info.html: ")
-        print(header)
-        header += L.decode()
-        header += "\n\n\n"
-        Client.send(bytes(header, 'utf-8'))
+    SendFile(Client, Request, GetFileNameFromRequest(Request))
 
-
-def SendInfoCSS(Server, Client):
-    Client, Request = ReadHTTPRequest(Server)
-    print("HTTP Request: ")
-    print(Request)
-    if "GET /styleInfo.css HTTP/1.1" in Request:
-        f = open("styleIndex.css", "rb")
-        L = f.read()
-        header = """HTTP/1.1 200 OK\r\nContent-Type: text/css; charset=UTF-8\r\nContent-Encoding: UTF-8\r\nContent-Length: %d\r\n\r\n"""%len(L)
-        print("-------------HTTP response  styleIndex.css: ")
-        print(header)
-        header += L.decode()
-        header += "\r\n"
-        Client.send(header.encode('utf-8')) 
-
-def SendImage(Server, Client):
-    Client, Request = ReadHTTPRequest(Server)
-    print("HTTP Request: ")
-    print(Request)
-    if "GET /image/ch.png HTTP/1.1" in Request:
-        f = open("image/ch.png", "rb")
+def SendFile(Client, Request, fileName):
+    if "GET /" + fileName + " HTTP/1.1" in Request:
+        f = open(fileName, "rb")
         L = f.read()
         f.close()
-        header = """HTTP/1.1 200 OK\r\nContent-Type: image/png\r\nContent-Length: %d\r\n\r\n"""%len(L)
-        print("-------------HTTP response  ch.png: ")
-        print(header)
-        header = header.encode('utf-8') + L + "\r\n".encode('utf-8')
-        Client.send(header)
+        extension = fileName.split('.')[1]
 
-    if "GET /image/qd.png HTTP/1.1" in Request:
-        f = open("image/qd.png", "rb")
-        L = f.read()
-        f.close()
-        header = """HTTP/1.1 200 OK\r\nContent-Type: image/png\r\nContent-Length: %d\r\n\r\n"""%len(L)
-        print("-------------HTTP response  qd.png: ")
+        header = "HTTP/1.1 200 OK\r\nContent-Type: "
+        if extension == "html" or extension == "css":
+            header += "text/"
+        else:
+            header += "image/"
+        header += extension
+        header += "\r\nContent-Length: %d\r\n\r\n""" % len(L)
+        print("-------------HTTP response " + fileName + ": ")
         print(header)
-        header = header.encode('utf-8') + L + "\r\n".encode('utf-8')
-        Client.send(header)
 
-    if "GET /favicon.ico HTTP/1.1" in Request:
-        f = open("image/ch.ico", "rb")
-        L = f.read()
-        f.close()
-        header = """HTTP/1.1 200 OK\r\nContent-Type: image/ico\r\nContent-Length: %d\r\n\r\n"""%len(L)
-        print("-------------HTTP response  favicon.ico: ")
-        print(header)
-        header = header.encode('utf-8') + L + "\r\n".encode('utf-8')
-        Client.send(header)
+        Client.send(header.encode('utf-8') + L + "\r\n".encode('utf-8'))
 
 if __name__ == "__main__":
     print("Part 1: Return our homepage when a client visit our Server")
-    # 1. Create server
+    # 1.  Create server
     Server = createServer("localhost", 1234)
 
-    # 2. Client connect to Server
+    # 2.  Client connect to Server
     Client, Request = ReadHTTPRequest(Server)
     print("-------------HTTP request: ")
     print(Request)
 
-    # 3. Response then close server
+    # 3.  Response then close server
     MoveHomePage(Server, Client, Request)
 
     print("Part 2: Username and password handling")
@@ -220,21 +112,21 @@ if __name__ == "__main__":
     print(Request)
 
     if CheckPass(Request) == True:
-        MoveInfo(Server, Client)
+        MoveToPage(Server, Client, "http://127.0.0.1:1236/info.html")
 
         Server = createServer("localhost", 1236)
-        SendInfo(Server, Client)
-        SendInfoCSS(Server, Client)
-        SendImage(Server, Client)
-        SendImage(Server, Client)
-        SendImage(Server, Client)
+        ReadRequestAndSendFile(Server) #info.html
+        ReadRequestAndSendFile(Server) #styleInfo.css
+        # 3 cái request dưới đây là ch.png, qd.png và favicon.ico, nhưng thứ tự không giữ nguyên mỗi lần chạy
+        ReadRequestAndSendFile(Server)
+        ReadRequestAndSendFile(Server)
+        ReadRequestAndSendFile(Server)
         Server.close()
 
     else:
-        Move404(Server, Client)
+        MoveToPage(Server, Client, "http://127.0.0.1:1236/404.html")
 
         Server = createServer("localhost", 1236)
-        Send404(Server, Client)
-        Send404CSS(Server, Client)
-
+        ReadRequestAndSendFile(Server)  #404.html
+        ReadRequestAndSendFile(Server)  #style404.css
         Server.close()
