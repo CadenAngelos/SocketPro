41dc2e5755b6d482416e62b07d1ea57fc34f2496
Caden <trinhde530@gmail.com>
2020-12-17 21:46:02 +0700

    not finish :((

--- Server.py
+++ Server.py
@@ -36,7 +36,7 @@ def ReadHTTPRequest(Server):
 def MoveHomePage(Server, Client, Request):
     if "GET /index.html HTTP/1.1" in Request:
         SendFile(Client, Request, GetFileNameFromRequest(Request))  # index.html
-        ReadRequestAndSendFile(Server)  # styleIndex.css
+        ReadRequestAndSendFile(Server, False)  # styleIndex.css
         Server.close()
         return True
     if "GET / HTTP/1.1" in Request:
@@ -63,6 +63,7 @@ def CheckPass(Request):
 def GetFileNameFromRequest(Request):
     return Request[5:Request.find(" HTTP/1.1")]  # GET /file.html HTTP/1.1...
 
+
 def MoveToPage(Server, Client, location):
     header = "HTTP/1.1 301 Moved Permanently\r\nLocation: "
     header += location
@@ -71,6 +72,7 @@ def MoveToPage(Server, Client, location):
     print(header)
     Client.send(bytes(header, 'utf-8'))
     Server.close()
+
 
 def ReadRequestAndSendFile(Server, isChunked):
 
@@ -84,6 +86,7 @@ def ReadRequestAndSendFile(Server, isChunked):
             SendChunkedFile(Client, Request, fileName)
         else:
             SendFile(Client, Request, fileName)
+
 
 def SendFile(Client, Request, fileName):
     f = open(fileName, "rb")
@@ -103,26 +106,28 @@ def SendFile(Client, Request, fileName):
 
     Client.send(header.encode('utf-8') + L + "\r\n".encode('utf-8'))
 
+
 def SendChunkedFile(Client, Request, fileName):
     splittedFileName = fileName.split('.')
-    extension = splittedFileName[len(splittedFileName) - 1] #lấy phần sau dấu chấm cuối cùng
+    # lấy phần sau dấu chấm cuối cùng
+    extension = splittedFileName[len(splittedFileName) - 1]
 
     header = "HTTP/1.1 200 OK\r\nContent-Type: "
     extensionMIMEType = {
-                'png':'image/png',
-                'jpg':'image/jpeg',
-                'jpeg':'image/jpeg',
-                'pptx':'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-                'ppt':'application/vnd.ms-powerpoint',
-                'pdf':'application/pdf',
-                'mp3':'audio/mpeg',
-                'mp4':'video/mp4',
-                'gif':'image/gif',
-                'wav':'audio/wav',
-                'docx':'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-                'xls':'application/vnd.ms-excel',
-                'xlsx':'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-             }
+        'png': 'image/png',
+        'jpg': 'image/jpeg',
+        'jpeg': 'image/jpeg',
+                'pptx': 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+                'ppt': 'application/vnd.ms-powerpoint',
+                'pdf': 'application/pdf',
+                'mp3': 'audio/mpeg',
+                'mp4': 'video/mp4',
+                'gif': 'image/gif',
+                'wav': 'audio/wav',
+                'docx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                'xls': 'application/vnd.ms-excel',
+                'xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    }
 
     header += extensionMIMEType.get(extension)
     header += "\r\nTransfer-Encoding: chunked\r\n\r\n"
@@ -135,19 +140,27 @@ def SendChunkedFile(Client, Request, fileName):
     fileBytes = "".encode('utf-8')
 
     while len(currentChunk) == BUFFER_SIZE:
-        fileBytes += ("%X\r\n" % BUFFER_SIZE).encode('utf-8') #kích thước của chunk phải truyền dưới dạng hex
+        # kích thước của chunk phải truyền dưới dạng hex
+        fileBytes += ("%X\r\n" % BUFFER_SIZE).encode('utf-8')
         fileBytes += currentChunk
         fileBytes += "\r\n".encode('utf-8')
         currentChunk = f.read(BUFFER_SIZE)
     f.close()
-    #phần còn lại
+    # phần còn lại
     remainingSize = len(currentChunk)
     fileBytes += ("%X\r\n" % remainingSize).encode('utf-8')
     fileBytes += currentChunk
     fileBytes += "\r\n".encode('utf-8')
-    fileBytes += ("%X\r\n" % 0).encode('utf-8')   #chunk kết thúc "0\r\n"
+    fileBytes += ("%X\r\n" % 0).encode('utf-8')  # chunk kết thúc "0\r\n"
 
     Client.send(header.encode('utf-8') + fileBytes + "\r\n".encode('utf-8'))
+
+
+def CheckButtonPress(Request):
+    if "POST / HTTP/1.1" in Request:
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
@@ -159,45 +172,46 @@ if __name__ == "__main__":
     Client, Request = ReadHTTPRequest(Server)
     print("-------------HTTP request: ")
     print(Request)
-    
-    #Test chunked
-    if "GET / HTTP/1.1" in Request:
-        MoveToPage(Server, Client, "http://127.0.0.1:1235/files.html")
-        Server.close()
-
-        Server = createServer("localhost", 1235)
-        ReadRequestAndSendFile(Server, False)   #files.html
-        ReadRequestAndSendFile(Server, False)   #favicon.ico
-        while True:
-            ReadRequestAndSendFile(Server, True)    #đợi cái href được bấm thì gửi đúng file
-
-        Server.close()
 
     # 3.  Response then close server
-    #MoveHomePage(Server, Client, Request)
+    MoveHomePage(Server, Client, Request)
 
-    #print("Part 2: Username and password handling")
-    #Server = createServer("localhost", 10000)
-    #Client, Request = ReadHTTPRequest(Server)
-    #print("-------------- HTTP request: ")
-    #print(Request)
+    print("Part 2: Username and password handling")
+    Server = createServer("localhost", 10000)
+    Client, Request = ReadHTTPRequest(Server)
+    print("-------------- HTTP request: ")
+    print(Request)
 
-    #if CheckPass(Request) == True:
-    #    MoveToPage(Server, Client, "http://127.0.0.1:1236/info.html")
+    if CheckPass(Request) == True:
+        MoveToPage(Server, Client, "http://127.0.0.1:1236/info.html")
 
-    #    Server = createServer("localhost", 1236)
-    #    ReadRequestAndSendFile(Server) #info.html
-    #    ReadRequestAndSendFile(Server) #styleInfo.css
-    #    # 3 cái request dưới đây là ch.png, qd.png và favicon.ico, nhưng thứ tự không giữ nguyên mỗi lần chạy
-    #    ReadRequestAndSendFile(Server)
-    #    ReadRequestAndSendFile(Server)
-    #    ReadRequestAndSendFile(Server)
-    #    Server.close()
+        Server = createServer("localhost", 1236)
+        ReadRequestAndSendFile(Server, False)  # info.html
+        ReadRequestAndSendFile(Server, False)  # styleInfo.css
+        # 3 cái request dưới đây là ch.png, qd.png và favicon.ico, nhưng thứ tự không giữ nguyên mỗi lần chạy
+        ReadRequestAndSendFile(Server, False)
+        ReadRequestAndSendFile(Server, False)
+        ReadRequestAndSendFile(Server, False)
+        Server.close()
 
-    #else:
-    #    MoveToPage(Server, Client, "http://127.0.0.1:1236/404.html")
+        Server = createServer("localhost", 10000)
+        Client, Request = ReadHTTPRequest(Server)
+        print("-------------- HTTP request: ")
+        print(Request)
+        if CheckButtonPress(Request) == True:
+            MoveToPage(Server, Client, "http://127.0.0.1:1237/files.html")
+            Server = createServer("localhost", 1237)
+            ReadRequestAndSendFile(Server, False)  # files.html
+            ReadRequestAndSendFile(Server, False)
+            ReadRequestAndSendFile(Server, False)  # favicon.ico
+            while True:
+                # gửi file được bấm theo kiểu chunked
+                ReadRequestAndSendFile(Server, True)
+        Server.close()
 
-    #    Server = createServer("localhost", 1236)
-    #    ReadRequestAndSendFile(Server)  #404.html
-    #    ReadRequestAndSendFile(Server)  #style404.css
-    #    Server.close()
+    else:
+        MoveToPage(Server, Client, "http://127.0.0.1:1236/404.html")
+        Server = createServer("localhost", 1236)
+        ReadRequestAndSendFile(Server, False)  # 404.html
+        ReadRequestAndSendFile(Server, False)  # style404.css
+        Server.close()
