93e51d2e5eab79ae7207d7ce6929d6ed936f8dbb
hachihao792001 <hachihao792001@gmail.com>
2020-12-17 15:12:59 +0700

    transfer-encoding: chunked XONG cơ chế

--- Server.py
+++ Server.py
@@ -1,5 +1,6 @@
 # #%% console shift enter
 import socket
+import time
 
 def createServer(host, port):
     Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -67,30 +68,70 @@ def MoveToPage(Server, Client, location):
     Client.send(bytes(header, 'utf-8'))
     Server.close()
 
-def ReadRequestAndSendFile(Server):
+def ReadRequestAndSendFile(Server, isChunked):
     Client, Request = ReadHTTPRequest(Server)
     print("-------------HTTP Request: ")
     print(Request)
-    SendFile(Client, Request, GetFileNameFromRequest(Request))
+
+    if "GET /" in Request and " HTTP/1.1" in Request:
+        fileName = GetFileNameFromRequest(Request)
+        if isChunked:
+            SendChunkedFile(Client, Request, fileName)
+        else:
+            SendFile(Client, Request, fileName)
 
 def SendFile(Client, Request, fileName):
-    if "GET /" + fileName + " HTTP/1.1" in Request:
-        f = open(fileName, "rb")
-        L = f.read()
-        f.close()
-        extension = fileName.split('.')[1]
+    f = open(fileName, "rb")
+    L = f.read()
+    f.close()
+    extension = fileName.split('.')[1]
 
-        header = "HTTP/1.1 200 OK\r\nContent-Type: "
-        if extension == "html" or extension == "css":
-            header += "text/"
-        else:
-            header += "image/"
-        header += extension
-        header += "\r\nContent-Length: %d\r\n\r\n""" % len(L)
-        print("-------------HTTP response " + fileName + ": ")
-        print(header)
+    header = "HTTP/1.1 200 OK\r\nContent-Type: "
+    if extension == "html" or extension == "css":
+        header += "text/"
+    else:
+        header += "image/"
+    header += extension
+    header += "\r\nContent-Length: %d\r\n\r\n""" % len(L)
+    print("-------------HTTP response " + fileName + ": ")
+    print(header)
 
-        Client.send(header.encode('utf-8') + L + "\r\n".encode('utf-8'))
+    Client.send(header.encode('utf-8') + L + "\r\n".encode('utf-8'))
+
+def SendChunkedFile(Client, Request, fileName):
+    extension = fileName.split('.')[1]
+
+    header = "HTTP/1.1 200 OK\r\nContent-Type: "
+    if extension == "pdf":
+        header += "application/"
+    elif extension == "html" or extension == "css" or extension == "pptx":
+        header += "text/"
+    else:
+        header += "image/"
+    header += extension
+    header += "\r\nTransfer-Encoding: chunked\r\n\r\n"
+    print("-------------HTTP response " + fileName + ": ")
+    print(header)
+
+    BUFFER_SIZE = 2048
+    f = open(fileName, "rb")
+    currentChunk = f.read(BUFFER_SIZE)
+    fileBytes = "".encode('utf-8')
+
+    while len(currentChunk) == BUFFER_SIZE:
+        fileBytes += ("%X\r\n"%BUFFER_SIZE).encode('utf-8') #kích thước của chunk phải truyền dưới dạng hex
+        fileBytes += currentChunk
+        fileBytes += "\r\n".encode('utf-8')
+        currentChunk = f.read(BUFFER_SIZE)
+    f.close()
+    #phần còn lại
+    remainingSize = len(currentChunk)
+    fileBytes += ("%X\r\n"%remainingSize).encode('utf-8')
+    fileBytes += currentChunk
+    fileBytes += "\r\n".encode('utf-8')
+    fileBytes += ("%X\r\n"%0).encode('utf-8')   #chunk kết thúc "0\r\n"
+
+    Client.send(header.encode('utf-8') + fileBytes + "\r\n".encode('utf-8'))
 
 if __name__ == "__main__":
     print("Part 1: Return our homepage when a client visit our Server")
@@ -101,32 +142,46 @@ if __name__ == "__main__":
     Client, Request = ReadHTTPRequest(Server)
     print("-------------HTTP request: ")
     print(Request)
+    
+    #Test chunked
+    if "GET / HTTP/1.1" in Request:
+        MoveToPage(Server, Client, "http://127.0.0.1:1235/files.html")
+        Server.close()
+
+        Server = createServer("localhost", 1235)
+        ReadRequestAndSendFile(Server, False)   #files.html
+        ReadRequestAndSendFile(Server, False)   #favicon.ico
+        while True:
+            ReadRequestAndSendFile(Server, True)    #đợi cái href được bấm thì gửi đúng file
+        Server.close()
+
+
 
     # 3.  Response then close server
-    MoveHomePage(Server, Client, Request)
+    #MoveHomePage(Server, Client, Request)
 
-    print("Part 2: Username and password handling")
-    Server = createServer("localhost", 10000)
-    Client, Request = ReadHTTPRequest(Server)
-    print("-------------- HTTP request: ")
-    print(Request)
+    #print("Part 2: Username and password handling")
+    #Server = createServer("localhost", 10000)
+    #Client, Request = ReadHTTPRequest(Server)
+    #print("-------------- HTTP request: ")
+    #print(Request)
 
-    if CheckPass(Request) == True:
-        MoveToPage(Server, Client, "http://127.0.0.1:1236/info.html")
+    #if CheckPass(Request) == True:
+    #    MoveToPage(Server, Client, "http://127.0.0.1:1236/info.html")
 
-        Server = createServer("localhost", 1236)
-        ReadRequestAndSendFile(Server) #info.html
-        ReadRequestAndSendFile(Server) #styleInfo.css
-        # 3 cái request dưới đây là ch.png, qd.png và favicon.ico, nhưng thứ tự không giữ nguyên mỗi lần chạy
-        ReadRequestAndSendFile(Server)
-        ReadRequestAndSendFile(Server)
-        ReadRequestAndSendFile(Server)
-        Server.close()
+    #    Server = createServer("localhost", 1236)
+    #    ReadRequestAndSendFile(Server) #info.html
+    #    ReadRequestAndSendFile(Server) #styleInfo.css
+    #    # 3 cái request dưới đây là ch.png, qd.png và favicon.ico, nhưng thứ tự không giữ nguyên mỗi lần chạy
+    #    ReadRequestAndSendFile(Server)
+    #    ReadRequestAndSendFile(Server)
+    #    ReadRequestAndSendFile(Server)
+    #    Server.close()
 
-    else:
-        MoveToPage(Server, Client, "http://127.0.0.1:1236/404.html")
+    #else:
+    #    MoveToPage(Server, Client, "http://127.0.0.1:1236/404.html")
 
-        Server = createServer("localhost", 1236)
-        ReadRequestAndSendFile(Server)  #404.html
-        ReadRequestAndSendFile(Server)  #style404.css
-        Server.close()
+    #    Server = createServer("localhost", 1236)
+    #    ReadRequestAndSendFile(Server)  #404.html
+    #    ReadRequestAndSendFile(Server)  #style404.css
+    #    Server.close()
